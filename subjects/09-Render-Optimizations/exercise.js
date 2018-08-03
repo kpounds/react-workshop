aca5c79d7e6490f80272beedcddafda61ed2d120
Keith Pounds <keith.pounds@autozone.com>
2018-08-03 16:34:11 -0500

    exercise 9

--- subjects/09-Render-Optimizations/exercise.js
+++ subjects/09-Render-Optimizations/exercise.js
@@ -24,21 +24,49 @@ class ListView extends React.Component {
     renderRowAtIndex: PropTypes.func.isRequired
   };
 
+  state = { scrollTop: 0, availableHeight: 0 };
+
+  componentDidMount() {
+    this.setState({
+      availableHeight: this.node.clientHeight
+    });
+  }
+
+  handleScroll = event => {
+    this.setState({ scrollTop: event.target.scrollTop });
+  };
+
   render() {
+    const { scrollTop, availableHeight } = this.state;
     const { numRows, rowHeight, renderRowAtIndex } = this.props;
     const totalHeight = numRows * rowHeight;
 
     const items = [];
 
-    let index = 0;
-    while (index < numRows) {
+    const startIndex = Math.floor(scrollTop / rowHeight);
+    const endIndex = Math.min(
+      startIndex + Math.ceil(availableHeight / rowHeight),
+      numRows
+    );
+
+    let index = startIndex;
+    while (index < endIndex) {
       items.push(<li key={index}>{renderRowAtIndex(index)}</li>);
       index++;
     }
 
     return (
-      <div style={{ height: "100vh", overflowY: "scroll" }}>
-        <div style={{ height: totalHeight }}>
+      <div
+        onScroll={this.handleScroll}
+        style={{ height: "100vh", overflowY: "scroll" }}
+        ref={node => (this.node = node)}
+      >
+        <div
+          style={{
+            height: totalHeight,
+            paddingTop: startIndex * rowHeight
+          }}
+        >
           <ol>{items}</ol>
         </div>
       </div>
@@ -48,7 +76,7 @@ class ListView extends React.Component {
 
 ReactDOM.render(
   <ListView
-    numRows={500}
+    numRows={500000}
     rowHeight={RainbowListDelegate.rowHeight}
     renderRowAtIndex={RainbowListDelegate.renderRowAtIndex}
   />,
